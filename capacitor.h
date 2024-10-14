dfe426728aa1ac7064e80fbd34ed3bf0ee56adbf
Jack Martin <jhmartin4510@triton.unm.edu>
2024-10-14 11:39:57 -0600

    Code outputs the correct values

--- capacitor.h
+++ capacitor.h
@@ -12,6 +12,7 @@ struct _capacitor
 	time = new double[50001];
 	voltage = new double[50001];
 	current = new double[50001];
+	C = 0.0000000001;
 	time[0] = 0;
 	voltage[0] = 0;
 	current[0] = 0.01;
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "capacitor.h"
+#include <iomanip>
 
 #define dT  0.0000000001
 #define STEP 50001
@@ -12,18 +13,17 @@
 int main()
 {
 	Capacitor c1;
-	c1.C = c;
 	for(int i = 1; i<STEP; i++)
 	{
 		c1.time[i] = c1.time[i-1]+dT;
-		c1.voltage[i] = c1.voltage[i-1]+((c1.current[i-1])*dT*(1/c));
-		c1.current[i] = c1.current[i-1]-(c1.current[i-1]*dT*(1/(R*c)));
+		c1.voltage[i] = c1.voltage[i-1]+((c1.current[i-1])*dT*(1/c1.C));
+		c1.current[i] = c1.current[i-1]-(c1.current[i-1]*dT*(1/(R*(c1.C))));
 	}
 	std::cout<<"Table of values: " <<std::endl;
-	std::cout<<"T_stamp\t\tTime\t\t\tVoltage\t\t\tCurrent" <<std::endl;
+	std::cout<<"T_stamp\tVoltage\tCurrent" <<std::endl;
 	for(int i = 1; i<251; i++)
 	{		
-		std::cout<<i <<"\t\t" <<c1.time[i*200] <<"\t\t\t" <<c1.voltage[i*200] <<"\t\t\t" <<c1.current[i*200] <<std::endl;
+		std::cout<<std::fixed <<std::setprecision(3) <<i*200 <<"\t" <<c1.voltage[i*200] <<"V\t" <<c1.current[i*200] <<"A" <<std::endl;
 	}
 	return 0;	
 }
