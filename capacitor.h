05569e933df39d153f0dd620d1121083c0b7ba1d
Jack Martin <jhmartin4510@triton.unm.edu>
2024-10-14 11:48:02 -0600

    With comments

--- capacitor.h
+++ capacitor.h
@@ -9,14 +9,16 @@ struct _capacitor
 	double C;
 	
 	_capacitor(){
-	time = new double[50001];
+/*dynamically allocating an array doubles for time, voltage, and current*/
+	time = new double[50001];	
 	voltage = new double[50001];
 	current = new double[50001];
-	C = 0.0000000001;
-	time[0] = 0;
-	voltage[0] = 0;
-	current[0] = 0.01;
+	C = 0.0000000001;	//initializing C to capacitor value
+	time[0] = 0;		//initialziing beginning of time array to 0
+	voltage[0] = 0;		//initializing beginning of voltage array to 0
+	current[0] = 0.01;	//initializing beginnign of current arrya to 0.01
 	}	
+/*destructor for a capacitor object. Frees the memory allocated for time, voltage, and current*/
 	~_capacitor(){
 	delete[] time;
 	delete[] voltage;
--- main.cpp
+++ main.cpp
@@ -4,21 +4,22 @@
 
 #define dT  0.0000000001
 #define STEP 50001
-#define R 1000
-#define c 0.0000000001 
+#define R 1000 
 #define I_const 0.01
 #define V_const 100 
 
 
 int main()
 {
-	Capacitor c1;
+	Capacitor c1;			//initialize a Capacitor struct 
+/*This loop calculates the voltage and current at 50000 time steps given the one is constant. It populates the array of members of the struct with the values of voltage, current, and time.*/
 	for(int i = 1; i<STEP; i++)
 	{
 		c1.time[i] = c1.time[i-1]+dT;
 		c1.voltage[i] = c1.voltage[i-1]+((c1.current[i-1])*dT*(1/c1.C));
 		c1.current[i] = c1.current[i-1]-(c1.current[i-1]*dT*(1/(R*(c1.C))));
 	}
+/*Outputting the Values of timestamp, voltage and current in table form*/
 	std::cout<<"Table of values: " <<std::endl;
 	std::cout<<"T_stamp\tVoltage\tCurrent" <<std::endl;
 	for(int i = 1; i<251; i++)
